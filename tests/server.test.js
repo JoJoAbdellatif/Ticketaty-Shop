1e465b51e784aaed58c39ac5e449afcf1c35e1c7
moeyad7 <mohamed.eyad2612@gmail.com>
2022-12-20 16:32:53 +0200

    updated

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,18 +2,16 @@ const request = require("supertest")
 const matchApiStub = require('./match.stub.json');
 const baseURL = "http://localhost:3000"
 
-const matchId = "639c7980d4aadd7d8786182a"
-
 describe("GET Match",()=>{
 
     it("Should return 200", async()=>{
-        const response = await request(baseURL).get("/matches/" + matchId);
+        const response = await request(baseURL).get(`/matches/${matchApiStub._id}`);
         expect(response.statusCode).toBe(200);
 
     })
 
     it("Should be equal to the stub match", async()=>{
-        const response = await request(baseURL).get("/matches/" + matchId);
+        const response = await request(baseURL).get(`/matches/${matchApiStub._id}`);
         expect(response.body).toEqual(matchApiStub); 
         
     })
