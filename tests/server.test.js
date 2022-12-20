26abb141c3026666dc49c8b7888c7e47820cb615
moeyad7 <mohamed.eyad2612@gmail.com>
2022-12-20 16:41:08 +0200

    updated get tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,7 +2,7 @@ const request = require("supertest")
 const matchApiStub = require('./match.stub.json');
 const baseURL = "http://localhost:3000"
 
-describe("GET Match",()=>{
+describe("GET Matches",()=>{
 
     it("Should return 200", async()=>{
         const response = await request(baseURL).get(`/matches/${matchApiStub[0]._id}`);
@@ -10,10 +10,27 @@ describe("GET Match",()=>{
 
     })
 
-    it("Should be equal to the stub match", async()=>{
+    it("Should be equal to the first match", async()=>{
         const response = await request(baseURL).get(`/matches/${matchApiStub[0]._id}`);
         expect(response.body).toEqual(matchApiStub[0]); 
         
     })
 
+    it("Should return 200", async()=>{
+        const response = await request(baseURL).get("/matches");
+        expect(response.statusCode).toBe(200);
+        
+    })
+
+    it("Should be equal to the first 10 matches", async()=>{
+        const response = await request(baseURL).get("/matches");
+        expect(response.body).toEqual(matchApiStub); 
+        
+    })
+
 })
+
+
+// describe("Search for Matches",() => {
+
+// })
